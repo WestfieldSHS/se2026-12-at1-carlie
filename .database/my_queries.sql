7fe51b4bbeb02b2968c953860ba16297c4184ef1
Carlie-Weatherby <acornerofthegalaxy@gmail.com>
2025-12-02 14:14:44 +1100

    Database set up for start of data entry

---
+++ .database/my_queries.sql
@@ -0,0 +1,19 @@
+-- *** SQL COMMANDS I'VE PLAYED WITH THROUGHOUT PROJECT (NOT IN ORDER) ***
+-- DROP TABLE birdFamily;
+-- ALTER TABLE birdFamily RENAME COLUMN family_name TO family_name_english;
+-- ALTER TABLE birdFamily RENAME COLUMN common_family_name TO family_name_latin;
+-- ALTER TABLE birdFamily RENAME TO birdFamilies;
+-- UPDATE birdFamilies SET family_name_latin = 'Artamidae' WHERE family_id = 1003;
+-- ALTER TABLE birdSpecies RENAME COLUMN jouvenile TO juvenile;
+-- DROP TABLE birdSpecies;
+-- DELETE FROM birdGenera WHERE genus_id = 1009;
+-- UPDATE birdSpecies SET genus_id = 1008 WHERE species_id = 3005;
+-- UPDATE birdSpecies SET image_bird2_desc = 'Midshot view of an adult Laughing Kookaburra, left profile view.' 
+-- WHERE species_id = 3001;
+-- DROP TABLE sightings;
+
+
+-- ************** CREATING AND INSERTING DATA TO TABLES **************
+
+
+-- ************** MY SQL QUERIES **************
